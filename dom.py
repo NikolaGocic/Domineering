2be5f9311f4bf83b6c1e7325bfbb0fbf31b559cd
Nikola Gocic <ngocic97@gmail.com>
2022-11-24 14:52:37 +0100

    Dodata provera za kraj igre, dodate sve funkcije za fazu 1

--- dom.py
+++ dom.py
@@ -7,10 +7,21 @@ naPotezu = 'X'
 brojVrsta = 0
 brojKolona = 0
 
+#Implementirati funkcije koje obezbeđuju unos početnih parametara igre
 def init():
     global matrica
+    global brojVrsta
+    global brojKolona
+
+    print("Unesite broj vrsta:")
+    brojVrsta = int(sys.stdin.readline())
+
+    print("Unesite broj kolona:")
+    brojKolona = int(sys.stdin.readline())
+
     matrica = [ [ " " for i in range(brojKolona) ] for j in range(brojVrsta) ]
 
+#Implementirati funkcije koje obezbeđuju prikaz proizvoljnog stanja problema (igre)
 def stampaj():
     top(brojKolona)
 
@@ -42,6 +53,8 @@ def bottom(brojKolona):
         print(' '+string.ascii_uppercase[num],end='')
     print('')
 
+
+#Realizovati funkcije koje proveravaju da li je potez valjan
 # vrsta 1..N | kolona A..Z
 def potezValjan(vrsta,kolona): 
     kol = ord(kolona)-65
@@ -64,6 +77,7 @@ def potezValjan(vrsta,kolona):
         else:
             return False
 
+#Realizovati funkcije koje na osnovu zadatog poteza igrača menjaju trenutno stanjeproblema (igre) i prelaze u novo
 def odigrajPotez(vrsta,kolona):
     global matrica
     global naPotezu
@@ -83,11 +97,37 @@ def odigrajPotez(vrsta,kolona):
     else:
         print("Uneli ste nevalidni potez!")
 
-def start():
-    global matrica
+#Napisati funkcije za proveru kraja igre
+def krajIgre():
+    
+    if(naPotezu=='X'):
+        vrs=0
+        kol=0
+        while(kol<brojKolona):
+            while(vrs<brojVrsta-1):
+                if(matrica[vrs][kol]==' ' and matrica[vrs+1][kol]==' '):
+                    return False
+                else:
+                    vrs+=1
+            kol+=1
+    
+    else:
+        vrs=0
+        kol=0
+        while(vrs<brojVrsta):
+            while(kol<brojKolona-1):
+                if(matrica[vrs][kol]==' ' and matrica[vrs][kol+1]==' '):
+                    return False
+                else:
+                    kol+=1
+            vrs+=1
+    
+    return True #Kraj je igre zato sto nema slobodnih polja za igraca koj je na potezu
+
+
+#Omogućiti izbor ko će igrati prvi (čovek ili računar)
+def koIgraPrvi():
     global igraPrvi
-    global brojVrsta
-    global brojKolona
 
     print("Ko igra prvi? (0-covek, 1-racunar):")
     igraPrvi = int(sys.stdin.readline())
@@ -96,19 +136,32 @@ def start():
     else:
         print("Racunar je prvi na potezu!")
 
-    print("Unesite broj vrsta:")
-    brojVrsta = int(sys.stdin.readline())
 
-    print("Unesite broj kolona:")
-    brojKolona = int(sys.stdin.readline())
+def start():
+
+    koIgraPrvi()
 
     init()
+
     stampaj()
 
     while True:
+        if(krajIgre()):
+            pobedio=""
+            if(naPotezu=='X'):
+                pobedio='O'
+            else:
+                pobedio='X'
+            print("Kraj Igre, pobedio je igrac "+pobedio+"!!!")
+            break
+
+
+        print("\nIgrac "+naPotezu+" na potezu")
+        print("Unesite vrstu (broj 1..N):")
         vrsta = int(sys.stdin.readline())
         if vrsta == -1:
             break
+        print("Unesite kolonu (slovo A..Z):")
         kolona = sys.stdin.readline()
         odigrajPotez(vrsta,kolona[0])
 
