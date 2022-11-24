e84332bb25d0d61cfd25400717bb71c5d83a2c09
Nikola Gocic <ngocic97@gmail.com>
2022-11-24 13:49:53 +0100

    Dodate funkcije potezValjan i odigrajPotez

--- dom.py
+++ dom.py
@@ -1,8 +1,11 @@
-
 import string
 import sys
 
 matrica = []
+igraPrvi = 0
+naPotezu = 'X'
+brojVrsta = 0
+brojKolona = 0
 
 def init():
     global matrica
@@ -18,7 +21,6 @@ def stampaj():
         print("|"+str(brojVrsta-x))
         
     bottom(brojKolona)    
-
 
 def top(brojKolona):
     print('  ',end='')
@@ -40,20 +42,75 @@ def bottom(brojKolona):
         print(' '+string.ascii_uppercase[num],end='')
     print('')
 
+# vrsta 1..N | kolona A..Z
+def potezValjan(vrsta,kolona): 
+    kol = ord(kolona)-65
+    vrs = brojVrsta-vrsta
+    
+    if(naPotezu=='X'):
+        if(vrs+1>=brojVrsta or kol>=brojKolona):
+            return False
 
-print("Unesite broj vrsta:")
-brojVrsta = int(sys.stdin.readline())
-print("Unesite broj kolona:")
-brojKolona = int(sys.stdin.readline())
-init()
-stampaj()
+        if(matrica[vrs][kol]==' ' and matrica[vrs+1][kol]==' '):
+            return True
+        else:
+            return False
+    else:
+        if(vrs>=brojVrsta or kol+1>=brojKolona):
+            return False
 
-while True:
-    vrsta = int(sys.stdin.readline())
-    if vrsta == -1:
-        break
-    kolona = int(sys.stdin.readline())
-    znak = sys.stdin.readline()
-    matrica[vrsta][kolona]=znak[0]
+        if(matrica[vrs][kol]==' ' and matrica[vrs][kol+1]==' '):
+            return True
+        else:
+            return False
+
+def odigrajPotez(vrsta,kolona):
+    global matrica
+    global naPotezu
+
+    if(potezValjan(vrsta,kolona)):
+        kol = ord(kolona)-65
+        vrs = brojVrsta-vrsta
+        if(naPotezu=='X'):
+            matrica[vrs][kol]='X'
+            matrica[vrs+1][kol]='X'
+            naPotezu='O'
+        else:
+            matrica[vrs][kol]='O'
+            matrica[vrs][kol+1]='O'
+            naPotezu='X'
+        stampaj()
+    else:
+        print("Uneli ste nevalidni potez!")
+
+def start():
+    global matrica
+    global igraPrvi
+    global brojVrsta
+    global brojKolona
+
+    print("Ko igra prvi? (0-covek, 1-racunar):")
+    igraPrvi = int(sys.stdin.readline())
+    if(igraPrvi==0):
+        print("Covek je prvi na potezu!")
+    else:
+        print("Racunar je prvi na potezu!")
+
+    print("Unesite broj vrsta:")
+    brojVrsta = int(sys.stdin.readline())
+
+    print("Unesite broj kolona:")
+    brojKolona = int(sys.stdin.readline())
+
+    init()
     stampaj()
 
+    while True:
+        vrsta = int(sys.stdin.readline())
+        if vrsta == -1:
+            break
+        kolona = sys.stdin.readline()
+        odigrajPotez(vrsta,kolona[0])
+
+
+start()
